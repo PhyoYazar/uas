7196042b8fd6e533144474441f9d78d4faecbe09
Phyo Yarzar <phyoyazar592@gmail.com>
2024-07-16 21:12:56 +0630

    fix: co ga grading api filter academicYear

--- business/core/vstudentmark/order.go
+++ business/core/vstudentmark/order.go
@@ -3,7 +3,7 @@ package vstudentmark
 import "github.com/PhyoYazar/uas/business/data/order"
 
 // DefaultOrderBy represents the default way we sort.
-var DefaultOrderBy = order.NewBy(OrderByID, order.ASC)
+var DefaultOrderBy = order.NewBy(OrderByRollNumber, order.ASC)
 
 // Set of fields that the results can be ordered by. These are the names
 // that should be used by the application layer.
